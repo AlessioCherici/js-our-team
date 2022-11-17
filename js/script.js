d65a820d127a8d9039a39b44fe9ea8bca45bc5dd
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-17 16:00:31 +0100

    Milestone 0  completata

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let arrayTeam = [
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        img: " wayne-barnett-founder-ceo.jpg",
+    },
+
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief editor",
+        img: "angela-caroll-chief-editor.jpg",
+    },
+
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        img: "walter-gordon-office-manager.jpg",
+    },
+
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg",
+    },
+
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        img: "scott-estrada-developer.jpg",
+    },
+
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphics Designer",
+        img: "barbara-ramos-graphic-designer.jpg",
+    }
+];
